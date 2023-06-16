4cd62d8a84458a25ba13d7f04f460d587db487ac
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-17 00:30:13 +0600

    app colors added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/views/home/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: CalculatorScreen(),
     );
   }
 }
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color operatorColor = Color(0xff272727);
+  static const Color buttonColor = Color(0xff191919);
+  static const Color orangeColor = Color(0xffd9802e);
+}
